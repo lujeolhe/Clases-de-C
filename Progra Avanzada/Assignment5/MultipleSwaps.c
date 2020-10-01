07f63229d5511064dd180cf6dd115a4f9d6eb9d1
lujeolhe <lujeolhe@live.com.mx>
2020-09-30 22:23:49 -0500

    multiple swaps

--- Progra Avanzada/Assignment5/MultipleSwaps.c
+++ Progra Avanzada/Assignment5/MultipleSwaps.c
@@ -1,74 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 
-void cambiarABinarioDe32Bits(unsigned int n, char* cadena, int* length){
-  unsigned long masc = 0x80000000;
-  for (int i = 0; i < 32; i++) {
-    if((masc >> i)& n){
-      *(cadena + *length) = ('1');
-      (*length) ++;
-
-    }
-    else{
-      *(cadena + *length) = ('0');
-      (*length)++;
-
-
-    }
-  }
-}
-
-int bitADecimal(char* cadena, int length){
-  int decimal = 0;
-  int exponente = 0;
-  for(int i = length-1; i >= 0; i--){
-    if(*(cadena + i) == '1'){
-      decimal+= pow(2, exponente);
-    }
-    exponente++;
-  }
-  return decimal;
-}
-
 int swapBits(int n1, int n2, int n3){
+  unsigned long uln1=(unsigned long)n1;
+  unsigned long masc  = 0x00000001;
+  unsigned long masc2 = 0x00000001;
 
-  char cadena[33];
-  for(int i = 0; i < 33; i++){
-    cadena[i] = 0;
-  }
+  masc = masc<<(n2-1);
+  masc2 = masc<<(n3-1);
 
-  int length = 0;
+  //printf("%x: %x %x\n",n1,masc,masc2 );
 
-  cambiarABinarioDe32Bits(n1, cadena, &length);
-  int n2O = n2;
-  int n3O = n3;
-  n2 = length - 1 - n2;
-  n3 = length -1 - n3;
-
-  int aux = n3O - n2O;
+  int aux = n3 - n2;
   if(aux % 2 == 0){
     aux /= 2;
   }
-
   else{
     aux = (aux - 1) / 2;
   }
 
-  for(int i = 0; i < aux; i++){
-    char cn2 = cadena[n2];
-    char cn3 = cadena[n3];
-
-    cadena[n2] = cn3;
-    cadena[n3] = cn2;
-     n2--;
-     n3++;
+  for(int i = 0; i <= aux; i++){
+    unsigned long cn2 = n1&(masc<<i);
+    unsigned long cn3 = n1&(masc2>>i);
+    //n1=n1&~(masc<<i|masc2>>i);
+    //printf("%x: %x %x %x |",n1,masc<<i,masc2>>i, masc<<i|masc2>>i);
+    cn2=cn2<<(n3-n2-2*i);
+    cn3=cn3>>(n3-n2-2*i);
+    //printf(" %x %x | %d\n",cn2, cn3,n3-n2-2*i );
+    n1=n1|cn2|cn3;
   }
-
-
-
-  return bitADecimal(cadena, length);
-
-
+  return n1;
 }
 
 
